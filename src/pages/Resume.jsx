7f7ca2cadbbad99ab0c58dc6a237aa06297254e1
MauryIV <mauryhughesiv@gmail.com>
2024-05-11 18:35:02 -0600

    Added resume, completed Resume page.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,39 @@
+import MyResume from '../assets/MauryHughesResume.pdf';
+
 export default function Resume() {
   return (
     <div className="container pt-4">
-      <p>
-        Resume
-      </p>
-      {/* link to download resume then a list of languages and libraries separated by front and back end */}
+      <a href={MyResume} download>
+        Feel free to download my resume.
+      </a>
+      <h2 className="pt-2">Skills</h2>
+      <h3>Front-end</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+        <li>Bulma</li>
+        <li>Git</li>
+        <li>Browser Developer Tools</li>
+        <li>Web APIs</li>
+        <li>Third-Party APIs</li>
+        <li>Server-Side APIs</li>
+      </ul>
+      <h3>Back-end</h3>
+      <ul>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>Progressive Web Applications (PWA)</li>
+        <li>Model-View-Controller (MVC)</li>
+        <li>Object-Oriented Programming (OOP)</li>
+        <li>Object-Relational Mapping (ORM)</li>
+        <li>MERN</li>
+        <li>State</li>
+        <li>NoSQL (MongoDB)</li>
+        <li>SQL (PostgreSQL)</li>
+      </ul>
     </div>
   );
 }
