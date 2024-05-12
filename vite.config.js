359c3c8104a9f1b79995f9d8c360bc17382da090
MauryIV <mauryhughesiv@gmail.com>
2024-05-12 08:59:41 -0600

    deploy test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000,
+    port: 3000,
     open: true
   }
 })
