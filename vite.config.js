69f80d2ead3ed3d98e6c47462bda573aadd8a388
MauryIV <mauryhughesiv@gmail.com>
2024-06-14 00:56:53 -0600

    deployment troubleshooting

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    rewrites: [
-      {
-        source: "/(.*)",
-        destination: "/"
-      }
-    ] 
+    serve: {
+      base: "/",
+      fallback: "index.html"
+    }
   }
 });
